2b5299098b0dc6419ee02d10597aafa5d819c8dc
L-Andy <lutaayaandrew200@gmail.com>
2026-07-20 21:00:06 +0300

    Adding helpers

---
+++ helpers/time.formater.ts
@@ -0,0 +1,9 @@
+import moment from "moment";
+
+export const formatDate = (date: string | Date, format = "DD MMM YYYY") =>
+  moment(date).format(format);
+
+export const formatDateTime = (date: string | Date) =>
+  moment(date).format("DD MMM YYYY, HH:mm");
+
+export const fromNow = (date: string | Date) => moment(date).fromNow();
