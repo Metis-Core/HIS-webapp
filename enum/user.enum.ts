3ab785aee5819bbdb70c0a9b045d9c359cd2f8ef
abyesiza <abyesizajoel@gmail.com>
2026-07-23 20:06:42 +0300

    Check he user role enums . Added super admin and instead of other and staff i just expanded it. I  thnk we should be more specific . Also i have removed the look at the enums of the user status maybe we need rto add more i dont see where the pending status works

--- enum/user.enum.ts
+++ enum/user.enum.ts
@@ -1,15 +1,20 @@
 export enum UserRoleEnum {
+    SUPER_ADMIN = "super_admin",
     ADMIN = "admin",
     DOCTOR = "doctor",
     PATIENT = "patient",
-    STAFF = "staff",
-    OTHER = "other",
+    NURSE = 'nurse',
+    LAB_TECH = 'lab_tech',
+    PHARMACIST = 'pharmacist',
+    RECEPTIONIST = 'receptionist',
+    ACCOUNTANT = 'accountant',
 }
+
 
 export enum UserStatusEnum {
     ACTIVE = "active",
     INACTIVE = "inactive",
-    PENDING = "pending",
+    PENDING = "pending",     // Just wodering Andrew what is the use of the pending status. 
     BLOCKED = "blocked",
 }
 
