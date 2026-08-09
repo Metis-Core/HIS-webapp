9a667b2a8c30d31876078457c11c41caa3fa8a01
Andrew LUTAAYA <CAL1169514+totalen@admin.hubtotal.net>
2026-08-09 12:23:01 +0300

    Clean up

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import Header from '@/components/layout/header';
-import Sidebar from '@/components/layout/sidebar';
-
-export default function Home() {
-  return <div>dashboard</div>;
-}
--- app/patients/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { PatientsProvider } from './patients-provider';
-
-export default function PatientsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <PatientsProvider>{children}</PatientsProvider>;
-}
